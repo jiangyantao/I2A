1de9594ff5de63179a5400251dc3bd578aaeb18b
jiangyantao <jiangyt8990@163.com>
2017-04-17 16:45:18 +0800

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,20 @@
-def insertion_sort(A):
+#算法导论的第10页，插入排序，数组升序排列。
+def insertion_sort_UP(A):
 	for j in range(1,len(A)):
 		key = A[j]
 		i = j - 1
 		while A[i]>key:
+			A [i+1] = A[i]
+			i = i-1
+		A[i+1] = key
+	return A
+
+#算法导论的第10页，插入排序，数组降序排列。
+def insertion_sort_DOWN(A):
+	for j in range(1,len(A)):
+		key = A[j]
+		i = j - 1
+		while i>=0 and A[i]<key:
 			A [i+1] = A[i]
 			i = i-1
 		A[i+1] = key
